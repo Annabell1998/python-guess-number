8d72419b0192b6380c51abb861247b65007ce872
stiveen <estivenseguracarillo@gmail.com>
2015-09-22 10:17:07 -0600

    Check if the number is greater than the random number

--- application.py
+++ application.py
@@ -8,3 +8,7 @@ numero_aleatorio = randrange(1, 20) #Esta es la variable donde se guarda el nume
 
 adivina=int(raw_input("Enter a number from 1 to 20 :")) #numero que ingresa el usuario
 
+>>>>>>> 622751e6299602101fa3e6a16ab63484aa7fd24b
+
+ if adivina > numero_aleatorio:
+ 	print "you guss a number to hihg, please try again"
