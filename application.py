be5312478100eabba532326d60e673f0622131a0
Annabell1998 <anabella.am598@gmail.com>
2015-09-22 11:06:18 -0600

    spelling mistakes and if statement to adivina greater than 20(modified)

--- application.py
+++ application.py
@@ -7,10 +7,9 @@ numero_aleatorio = randrange(1, 20) #Esta es la variable donde se guarda el nume
 #count = 0 Nos servira en el ciclo While donde verificaremos To high y To Low
 
 adivina=int(raw_input("Enter a number from 1 to 20 :")) #numero que ingresa el usuario
-
-if adivina > numero_aleatorio:
+if adivina>20:
+	print "the number you entered is greater than 20"
+elif adivina > numero_aleatorio:
  	print "you guess a number to high, please try again"
 elif adivina < numero_aleatorio:
 	print "you guess a number to low ,please try aganin"
-elif adivina>20:
-	print "the number you entered is greater than 20"
