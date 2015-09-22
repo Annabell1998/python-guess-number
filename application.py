ffc9278eddad5a8bf98267a29c6872897482b0f6
Christofer17 <christopher.acevedo117@gmail.com>
2015-09-22 14:10:39 -0600

    I translated everything to English

--- application.py
+++ application.py
@@ -1,17 +1,13 @@
-# Aqui escribe tu codigo
-
 from random import randrange
 
-numero_aleatorio = randrange(1, 20) #Esta es la variable donde se guarda el numero aleatorio
+random_number = randrange(1, 20) #This is the variable where is stored the random number
 
-#count = 0 Nos servira en el ciclo While donde verificaremos To high y To Low
-
-adivina=int(raw_input("Enter a number from 1 to 20 :")) #numero que ingresa el usuario
-if adivina <=0:
+guess=int(raw_input("Enter a number from 1 to 20 :")) #number that enters the user
+if guess <=0:
 	print"your number is lower to 1"
-elif adivina>20:
+elif guess>20:
 	print "the number you entered is greater than 20"
-elif adivina > numero_aleatorio:
+elif guess > random_number:
  	print "you guess a number to high, please try again"
-elif adivina < numero_aleatorio:
-	print "you guess a number to low ,please try aganin"
+elif guess < random_number:
+	print "you guess a number to low ,please try again"
