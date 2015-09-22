e8da206f3925193af20f0da3f0e782f66a7d30b6
william1997 <marvinabelmon@gmail.com>
2015-09-22 10:34:39 -0600

    check if the number is lower than the random number

--- application.py
+++ application.py
@@ -11,4 +11,6 @@ adivina=int(raw_input("Enter a number from 1 to 20 :")) #numero que ingresa el u
 >>>>>>> 622751e6299602101fa3e6a16ab63484aa7fd24b
 
  if adivina > numero_aleatorio:
- 	print "you guss a number to hihg, please try again"
+ 	print "you guss a number to high, please try again"
+if adivina < numero_aleatorio:
+	print "you guess a numbre to low ,please try aganin"
