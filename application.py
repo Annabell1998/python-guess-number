622751e6299602101fa3e6a16ab63484aa7fd24b
Annabell1998 <anabella.am598@gmail.com>
2015-09-22 09:45:46 -0600

    Generates a random number and asks the user to enter a number from 1 to 20 generates a random number and asks the user to enter a number from 1 to 20
    ^O

--- application.py
+++ application.py
@@ -1,4 +1,10 @@
 # Aqui escribe tu codigo
 
-print "Hello World"
-print "hola"
+from random import randrange
+
+numero_aleatorio = randrange(1, 20) #Esta es la variable donde se guarda el numero aleatorio
+
+#count = 0 Nos servira en el ciclo While donde verificaremos To high y To Low
+
+adivina=int(raw_input("Enter a number from 1 to 20 :")) #numero que ingresa el usuario
+
