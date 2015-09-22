7f58ff1a63e98f97f2dc75e1e66078309a935ad4
Annabell1998 <anabella.am598@gmail.com>
2015-09-22 10:55:42 -0600

    indentation error

--- application.py
+++ application.py
@@ -8,9 +8,7 @@ numero_aleatorio = randrange(1, 20) #Esta es la variable donde se guarda el nume
 
 adivina=int(raw_input("Enter a number from 1 to 20 :")) #numero que ingresa el usuario
 
->>>>>>> 622751e6299602101fa3e6a16ab63484aa7fd24b
-
- if adivina > numero_aleatorio:
+if adivina > numero_aleatorio:
  	print "you guss a number to high, please try again"
 if adivina < numero_aleatorio:
-	print "you guess a numbre to low ,please try aganin"
+	print "you guess a number to low ,please try aganin"
