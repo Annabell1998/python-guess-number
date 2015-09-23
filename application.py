bae4a33bfea8023e08526c238a4ab1cb801e0815
Annabell1998 <anabella.am598@gmail.com>
2015-09-23 09:20:57 -0600

    Test 2

--- application.py
+++ application.py
@@ -1,16 +1,16 @@
-from random import randrange
-
-
-random_number = randrange(1, 21) #This is the variable where is stored the random number
-
+"""This is a comentary"""
+import random
+#This is the variable where is stored the random number
 try:
-	guess=int(raw_input("Enter a number from 1 to 20: ")) #number that enters the user
-	if guess <=0 or guess > 20:
-		print"You need to enter a number between 1 to 20"
-	elif guess > random_number:
- 		print "you guess a number to high, please try again"
-	elif guess < random_number:
-		print "you guess a number to low ,please try again"
-
-except:
-	print "You don't know what is a number?"
+    NUMBER = random.randrange(1, 21)
+    print NUMBER
+    GUESS = int(raw_input("Enter a number from 1 to 20: "))
+    #number that enters the user
+    if GUESS <= 0 or GUESS > 20:
+        print "You need to enter a number between 1 to 20"
+    elif GUESS > NUMBER:
+        print "you guess a number to high, please try again"
+    elif GUESS < NUMBER:
+        print "you guess a number to low, please try again"
+except ValueError:
+    print "You don't know what is a number?"
